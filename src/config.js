15a020fd79b56e57bb7d03b95d2902340d32bbbb
Oğuzhan Demir <doguzhan476@gmail.com>
2024-09-14 22:43:02 +0300

    update config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const baseUrl = "https://auth-api-beg6ftbghybrhabq.eastus-01.azurewebsites.net";
 const authProdUrl = "https://authapi.forsico.io";
 
-module.exports = {baseUrl}; 
+module.exports = {baseUrl,authProdUrl}; 
