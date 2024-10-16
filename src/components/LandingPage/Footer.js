25acb34705d8259b92d79d22ca0a29e2cd06ae43
alp.yurtseven <alpyurtseven99@gmail.com>
2024-10-16 14:23:19 +0300

    added contact service

--- src/components/LandingPage/Footer.js
+++ src/components/LandingPage/Footer.js
@@ -1,66 +1,143 @@
-import React from 'react';
-import '../../styles/Footer.css';
-import fullNameIcon from '../../assets/footer-fullname-icon.svg';
-import emailIcon from '../../assets/footer-email-icon.svg';
-import linkedInIcon from '../../assets/linkedin-icon.svg';
-import instagramIcon from '../../assets/instagram-icon.svg';
-import twitterIcon from '../../assets/x-icon.svg';
-
+import React, { useState } from "react";
+import "../../styles/Footer.css";
+import fullNameIcon from "../../assets/footer-fullname-icon.svg";
+import emailIcon from "../../assets/footer-email-icon.svg";
+import linkedInIcon from "../../assets/linkedin-icon.svg";
+import instagramIcon from "../../assets/instagram-icon.svg";
+import twitterIcon from "../../assets/x-icon.svg";
+import Support from "../../api/SupportApi/index";
 
 const Footer = () => {
-    return (
-        <footer className="footer-container">
-            <div className="footer-content">
-                <div className='background'></div>
-                <div className='footer-left-area'>
-                    <div className='left-area-title'>
-                        <h2>Contact us for more information</h2>
-                    </div>
-                    <div className='left-area-content'>
-                        <p>
-                            Regístrate y disfruta de todas las ventajas que te ofrecemos, introduce tus platos y empieza la transformación de tu restaurante.
-                        </p>
-                    </div>
-                </div>
-                <div className='footer-input-group'>
-                    <form>
-                        <div className="input-group">
-                            <img src={fullNameIcon} alt="fullname" />
-                            <input type="text" placeholder="Full name" required />
-                        </div>
+  const support = new Support();
+  const [ticketState, setTicketState] = useState({
+    email: "",
+    fullName: "",
+    content: "",
+    privacyAccepted: false,
+  });
 
-                        <div className="input-group">
-                            <img src={emailIcon} alt="email" />
-                            <input type="email" placeholder="E-mail address" required />
-                        </div>
+  const handleFullNameChange = (e) => {
+    setTicketState((previousState) => ({
+      ...previousState,
+      fullName: e.target.value || "",
+    }));
+  };
 
-                        <div className="input-group">
-                            <input className='message' placeholder="Message" required></input>
-                        </div>
+  const handleContentChange = (e) => {
+    setTicketState((previousState) => ({
+      ...previousState,
+      content: e.target.value || "",
+    }));
+  };
 
-                        <div className="privacy-policy ">
-                            <input type="checkbox" required />
-                            <label>I have read and accept the privacy policy.</label>
-                        </div>
+  const handleEmailChange = (e) => {
+    setTicketState((previousState) => ({
+      ...previousState,
+      email: e.target.value || "",
+    }));
+  };
 
-                        <button type="submit" className="send-btn">Send</button>
-                    </form>
-                </div>
+  const handlePrivacyChange = (e) => {
+    console.log("Privacy", e);
+    setTicketState((previousState) => ({
+      ...previousState,
+      privacyAccepted: e.target.checked || false,
+    }));
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    if (ticketState.privacyAccepted) {
+      await support.createContactTicket();
+    }
+  };
+
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <div className="background"></div>
+        <div className="footer-left-area">
+          <div className="left-area-title">
+            <h2>Contact us for more information</h2>
+          </div>
+          <div className="left-area-content">
+            <p>
+              Regístrate y disfruta de todas las ventajas que te ofrecemos,
+              introduce tus platos y empieza la transformación de tu
+              restaurante.
+            </p>
+          </div>
+        </div>
+        <div className="footer-input-group">
+          <form type="post" onSubmit={handleFormSubmit}>
+            <div className="input-group">
+              <img src={fullNameIcon} alt="fullname" />
+              <input
+                type="text"
+                value={ticketState.fullName}
+                onChange={handleFullNameChange}
+                placeholder="Full name"
+                required
+              />
             </div>
 
-            <div className="footer-bottom">
-                <div className="logo">
-                    <span href="#"><img src="./forsico-logo.svg" alt="logo" /></span>
-                </div>
-                <div className='footer-middle'><p>© 2024 Forsico. All rights reserved.</p></div>
-                <div className="social-icons">
-                    <span className='icon linkedin-icon' href="#"><img src={linkedInIcon} alt="linkedin" /></span>
-                    <span className='icon instagram-icon' href="#"><img src={instagramIcon} alt="instagram" /></span>
-                    <span className='icon linkedin-icon' href="#"><img src={twitterIcon} alt="x" /></span>
-                </div>
+            <div className="input-group">
+              <img src={emailIcon} alt="email" />
+              <input
+                type="email"
+                value={ticketState.email}
+                onChange={handleEmailChange}
+                placeholder="E-mail address"
+                required
+              />
             </div>
-        </footer>
-    );
+
+            <div className="input-group">
+              <input
+                className="message"
+                value={ticketState.content}
+                onChange={handleContentChange}
+                placeholder="Message"
+                required
+              ></input>
+            </div>
+
+            <div className="privacy-policy ">
+              <input onChange={handlePrivacyChange} type="checkbox" required />
+              <label>I have read and accept the privacy policy.</label>
+            </div>
+
+            <button type="submit" className="send-btn">
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <div className="logo">
+          <span href="#">
+            <img src="./forsico-logo.svg" alt="logo" />
+          </span>
+        </div>
+        <div className="footer-middle">
+          <p>© 2024 Forsico. All rights reserved.</p>
+        </div>
+        <div className="social-icons">
+          <span className="icon linkedin-icon" href="#">
+            <img src={linkedInIcon} alt="linkedin" />
+          </span>
+          <span className="icon instagram-icon" href="#">
+            <img src={instagramIcon} alt="instagram" />
+          </span>
+          <span className="icon linkedin-icon" href="#">
+            <img src={twitterIcon} alt="x" />
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 const baseUrl = "https://auth-api-beg6ftbghybrhabq.eastus-01.azurewebsites.net";
 const authProdUrl = "https://authapi.forsico.io";
+const supportApiUrl = "https://support.forsico.io";
+const supportApiKey = "bebabd708e56b530141a6fdae5aa35b4";
 
-module.exports = {baseUrl,authProdUrl}; 
+module.exports = { baseUrl, authProdUrl, supportApiUrl, supportApiKey };
